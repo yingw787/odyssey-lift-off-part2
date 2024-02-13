2b6e4b454d46f65734c628009f629971520e9acd
Ying Wang <yingw787.personal@gmail.com>
2024-02-13 17:50:41 -0500

    Update codegen.ts with DataSourceContext

--- server/codegen.ts
+++ server/codegen.ts
@@ -4,7 +4,10 @@ const config: CodegenConfig = {
   schema: "./src/schema.ts",
   generates: {
     "./src/types.ts": {
-      plugins: ["typescript", "typescript-resolvers"]
+      plugins: ["typescript", "typescript-resolvers"],
+      config: {
+        contextType: "./context#DataSourceContext"
+      }
     },
   }
 };
