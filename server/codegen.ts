c83b813a749184002ed732d591bc8bb007c3ca88
Ying Wang <yingw787.personal@gmail.com>
2024-02-13 17:53:15 -0500

    Update codegen.ts adding TrackModel mapper to code generation.

--- server/codegen.ts
+++ server/codegen.ts
@@ -6,7 +6,10 @@ const config: CodegenConfig = {
     "./src/types.ts": {
       plugins: ["typescript", "typescript-resolvers"],
       config: {
-        contextType: "./context#DataSourceContext"
+        contextType: "./context#DataSourceContext",
+        mappers: {
+          Track: "./models#TrackModel"
+        }
       }
     },
   }
